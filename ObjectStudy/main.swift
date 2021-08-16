f9340b603687534a3af4af9e9418e581c693defa
starpro123 <starpro123@naver.com>
2021-08-17 03:08:43 +0900

    주석달기

--- ObjectStudy/main.swift
+++ ObjectStudy/main.swift
@@ -12,21 +12,30 @@ print("a + b")
 print("a - b")
 print("a * b")
 print("a / b")
-let text = readLine()
-let expression = text!.components(separatedBy: " ")
 
-let a = expression[0]
-let oper = expression[1]
-let b = expression[2]
-switch oper {
-case "+":
-    print(Int(a)! + Int(b)!)
-case "-":
-    print(Int(a)! - Int(b)!)
-case "*":
-    print(Int(a)! * Int(b)!)
-case "/":
-    print(Double(a)! / Double(b)!)
-default:
-    break
+while true {
+    let text = readLine()
+    // text! : Swift의 Optional 변수를 Force Unwrapping 하기위해 사용
+    // Optional : nil(값이 존재하지 않음) or 실제 존재하는 값
+    // Swift의 타입은 nil 이라는 변수를 가질 수 없기때문에 존재하는 문법
+    // components(separatedBy: String) -> split 기능
+    let expression = text!.components(separatedBy: " ")
+
+    let a = expression[0]
+    let oper = expression[1]
+    let b = expression[2]
+
+    switch oper {
+    case "+":
+        print(Int(a)! + Int(b)!)
+    case "-":
+        print(Int(a)! - Int(b)!)
+    case "*":
+        print(Int(a)! * Int(b)!)
+    case "/":
+        print(Double(a)! / Double(b)!)
+    default:
+        break
+    }
 }
+

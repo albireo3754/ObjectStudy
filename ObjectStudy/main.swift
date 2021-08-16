fed5b58d5d47acc484cdf6a0e7d9f2186d131972
starpro123 <starpro123@naver.com>
2021-08-16 17:41:49 +0900

    최선을 다해 대충 만든 계산기

--- ObjectStudy/main.swift
+++ ObjectStudy/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+print("다음 방식중 하나로 입력해주세요")
+print("a + b")
+print("a - b")
+print("a * b")
+print("a / b")
+let text = readLine()
+let expression = text!.components(separatedBy: " ")
 
+let a = expression[0]
+let oper = expression[1]
+let b = expression[2]
+switch oper {
+case "+":
+    print(Int(a)! + Int(b)!)
+case "-":
+    print(Int(a)! - Int(b)!)
+case "*":
+    print(Int(a)! * Int(b)!)
+case "/":
+    print(Double(a)! / Double(b)!)
+default:
+    break
+}
